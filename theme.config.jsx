4466ad248e5a3b83bc2bf6937dfd163c51a5102a
lin-mt <lin-mt@outlook.com>
2023-12-24 22:27:50 +0800

    feat: init notes.

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+export default {
+    logo: <strong>Notes</strong>,
+    project: {
+        link: 'https://github.com/lin-mt/notes'
+    },
+    docsRepositoryBase: "https://github.com/lin-mt/notes",
+    feedback: {
+        content: null
+    },
+    footer: {
+        text: (
+            <span>
+                MIT {new Date().getFullYear()} ©{' '}<a href="https://github.com/lin-mt/notes" target="_blank">Notes</a>.
+            </span>
+        )
+    }
+}
