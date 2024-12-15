3e53a04e57b969b545aeecc199f56c30eba24af5
lin-mt <lin-mt@outlook.com>
2024-12-15 19:27:03 +0800

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://lin-mt.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/notes/',
+  baseUrl: process.env.BASE_URL ? process.env.BASE_URL : '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
